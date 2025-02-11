528ca65d293bafd45d776186a4665dc7f4e43d57
viethanggit2025 <viethang12292k2@gmail.com>
2025-02-11 18:48:45 +0700

    tạo componet share

--- VieHanStore/src/app/app.module.ts
+++ VieHanStore/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShareComponent } from './share/share.component';
+import { FooterComponent } from './share/footer/footer.component';
+import { HeaderComponent } from './share/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShareComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
